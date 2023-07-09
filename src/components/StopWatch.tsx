36c0c1b24e213a60400aac3f38602b26e6137c63
TRETAKOVYURA2009 <tretakovura2009@gmail.com>
2023-07-09 18:38:34 +0800

    stopwatch styles

--- src/components/StopWatch.tsx
+++ src/components/StopWatch.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react"
+import React, { useEffect, useMemo, useState } from "react"
 import { GameStatus } from "../types/game"
 
 interface StopWatchProps {
@@ -7,6 +7,14 @@ interface StopWatchProps {
 
 const StopWatch: React.FC<StopWatchProps> = ({ status }) => {
   const [counter, setCounter] = useState(0)
+  const timeUnits = useMemo(() => {
+    const minutes = Math.floor(counter / 60)
+    const seconds = Math.ceil(counter) % 60
+    return {
+      minutes,
+      seconds: `${minutes && seconds < 10 ? "0" : ""}${seconds}`,
+    }
+  }, [counter])
 
   useEffect(() => {
     if (status === GameStatus.started) {
@@ -16,7 +24,14 @@ const StopWatch: React.FC<StopWatchProps> = ({ status }) => {
     }
   }, [counter, status])
 
-  return <span className="mont text-lg">{Math.ceil(counter)}</span>
+  return (
+    <div className="border-2 rounded-lg border-gray-950 w-20 text-center">
+      {!!timeUnits.minutes && (
+        <span className="mont text-lg">{`${timeUnits.minutes}:`}</span>
+      )}
+      <span className="mont text-lg">{`${timeUnits.seconds}`}</span>
+    </div>
+  )
 }
 
 export default StopWatch
